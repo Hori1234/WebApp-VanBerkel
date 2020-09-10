30859341e3e6129be18819f4d955a1f7087e5573
Saltimbancul <32740860+Saltimbancul@users.noreply.github.com>
2020-09-10 11:23:18 +0300

    Revert "Added Sign In and Navigation View"
    This reverts commit 83e3ef38c84ebece5201b8a294e2be22ad51a1a7.

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import ButtonComponent from './components/ButtonComponent';
+
+function App() {
+  return (
+      <ButtonComponent/>
+      
+  );
+}
+
+export default App;
