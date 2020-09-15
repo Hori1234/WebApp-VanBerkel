e9dff58b7ea3568e0d2d509326d56860229ff294
Marco Luijten <m.luijten@student.tue.nl>
2020-09-15 10:43:19 +0200

    Added text to image buttons

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -7,38 +7,57 @@ import { Image } from 'antd';
 export default class Home extends Component {
     render() {
         return (
-            <Layout style={{alignItems: "center", display: "flex"}}>
-                <Layout style={{flexDirection: "row", justifyContent: "space-around", marginBottom: 50, padding: 25}}>
-                    <Image style={{marginRight: 50}}
-                        width={100}
-                        src={require("../Images/1635626-200.png")}
-                    />
-                    <Image
-                        width={100}
-                        src={require("../Images/upload-file.svg")}
-                    />
+            <Layout style={{flexDirection: "row", alignItems: "center", display: "flex"}}>
+                <Layout style={{justifyContent: "space-around", marginBottom: 50, padding: 20}}>
+                    <Layout style={{flexDirection: "row", alignItems: "center", display: "flex"}}>
+                        <Image style={{marginRight: 50, marginBottom: 50}}
+                            width={100}
+                            src={require("../Images/1635626-200.png")}
+                        />
+                        Account Management
+                    </Layout>
+
+                    <Layout style={{flexDirection: "row", alignItems: "center", display: "flex"}}>
+                        <Image style={{marginRight: 50, marginBottom: 50}}
+                            width={100}
+                            src={require("../Images/23_generate_value_for_investors_rapid_growth_planning-512.svg")}
+                        />
+                        Generate Planning
+                    </Layout>
+
+                    <Layout style={{flexDirection: "row", alignItems: "center", display: "flex"}}>
+                        <Image style={{marginRight: 50}}
+                           width={100}
+                           src={require("../Images/Data Visualisation.webp")}
+                        />
+                        Data Visualisation
+                    </Layout>
                 </Layout>
 
-                <Layout style={{flexDirection: "row", justifyContent: "space-around", marginBottom: 50, padding: 25}}>
-                    <Image style={{marginRight: 50, marginBottom: 50}}
-                        width={100}
-                        src={require("../Images/23_generate_value_for_investors_rapid_growth_planning-512.svg")}
-                    />
-                    <Image
-                        width={100}
-                        src={require("../Images/view-planning.svg")}
-                    />
-                </Layout>
+                <Layout style={{justifyContent: "space-around", marginBottom: 50, padding: 20}}>
+                    <Layout style={{flexDirection: "row", alignItems: "center", display: "flex"}}>
+                        <Image style={{marginRight: 50, marginBottom: 50}}
+                            width={100}
+                            src={require("../Images/upload-file.svg")}
+                        />
+                        Upload Files
+                    </Layout>
 
-                <Layout style={{flexDirection: "row", justifyContent: "space-around"}}>
-                    <Image style={{marginRight: 50, marginBottom: 50}}
-                        width={100}
-                        src={require("../Images/Data Visualisation.webp")}
-                    />
-                    <Image
-                        width={100}
-                        src={require("../Images/Data Analytics.webp")}
-                    />
+                    <Layout style={{flexDirection: "row", alignItems: "center", display: "flex"}}>
+                        <Image style={{marginRight: 50, marginBottom: 50}}
+                            width={100}
+                            src={require("../Images/view-planning.svg")}
+                        />
+                        View Planning
+                    </Layout>
+
+                    <Layout style={{flexDirection: "row", alignItems: "center", display: "flex"}}>
+                        <Image style={{marginRight: 50}}
+                            width={100}
+                            src={require("../Images/Data Analytics.webp")}
+                        />
+                        Data Analytics
+                    </Layout>
                 </Layout>
             </Layout>
         )
