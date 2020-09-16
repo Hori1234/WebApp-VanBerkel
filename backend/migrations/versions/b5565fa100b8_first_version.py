906bb8980e23e038659761aa389f56a0ef7d1631
Rick Luiken <rick-luiken@live.nl>
2020-09-16 13:32:27 +0200

    fix: Removed role column from the first version of the db, as it is added in another version

--- backend/migrations/versions/b5565fa100b8_first_version.py
+++ backend/migrations/versions/b5565fa100b8_first_version.py
@@ -22,7 +22,6 @@ def upgrade():
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('username', sa.String(length=40), nullable=False),
     sa.Column('password', sa.String(length=100), nullable=False),
-    sa.Column('role', sa.String(length=20), nullable=False),
     sa.PrimaryKeyConstraint('id'),
     sa.UniqueConstraint('username')
     )
