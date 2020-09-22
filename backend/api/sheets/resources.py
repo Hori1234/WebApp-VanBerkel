d2d8d9ff2dbdaa243b9f3cf746448c94d7b5a4ae
Rick Luiken <rick-luiken@live.nl>
2020-09-22 11:59:23 +0200

    Feat: Written a schema for the truck availability sheet.

--- backend/api/sheets/resources.py
+++ backend/api/sheets/resources.py
@@ -1,4 +1,7 @@
 from flask.views import MethodView
+from flask_smorest import abort
+import pandas as pd
+from xlrd import XLRDError
 from backend.extensions import roles_required
 from . import bp
 from .schemas import FileSchema
@@ -10,10 +13,19 @@ class Sheets(MethodView):
     @roles_required('planner', 'administrator')
     @bp.arguments(FileSchema, location='files')
     @bp.response(code=200)
+    @bp.response("BAD_REQUEST", code=400)
     def post(self, file):
         """
         Parses the orders and truck availability sheets files.
         """
         file_1 = file.pop('file_1')  # file_1 is required, so is always here
         file_2 = file.pop('file_2', None)
+
+        try:
+            df_1 = pd.read_excel(file_1)
+            if file_2:
+                df_2 = pd.read_excel(file_2)
+        except XLRDError:
+            abort(400, "File type not supported.")
+
         return '', 200
--- backend/api/sheets/schemas.py
+++ backend/api/sheets/schemas.py
@@ -1,4 +1,7 @@
 from backend.app import ma
+from marshmallow import validates
+from marshmallow.exceptions import ValidationError
+from marshmallow.fields import String, Integer, Boolean, Float, DateTime, Time
 from flask_smorest.fields import Upload
 
 
@@ -6,3 +9,31 @@ class FileSchema(ma.Schema):
     file_1 = Upload(required=True)
     file_2 = Upload()
 
+
+class TruckAvailabilitySchema(ma.Schema):
+
+    id = String(data_key='Truck ID', required=True)
+    s_number = Integer(data_key='Truck S No', required=True)
+    availability = Boolean(data_key='Availability of truck', required=True)
+    truck_type = String(data_key='Truck type', required=True)
+    business_type = String(data_key='business Type')
+    driver = String(data_key='Driver')
+    terminal = String(data_key='Terminal Base', required=True)
+    owner = String(data_key='Owner')
+    hierarchy = Float(data_key='Truck Hierarchy', required=True)
+    use_cost = Float(data_key='Truck Use Cost', required=True)
+    date = DateTime(data_key='Date', required=True)
+    starting_time = Time(data_key='Starting time', required=True)
+    remarks = String(data_key='Remarks', allow_none=True)
+
+    @validates('truck_type')
+    def validate_truck_type(self, val):
+        if val.lower() not in ['terminal', 'regional', 'port']:
+            raise ValidationError('Truck type must be one of '
+                                  'terminal, regional or port.')
+
+    @validates('terminal')
+    def validate_terminal(self, val):
+        if val.lower() not in ['itv', 'kat', 'oss']:
+            raise ValidationError('Terminal base must be one of '
+                                  'ITV, KAT or OSS.')
