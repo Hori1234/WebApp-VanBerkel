9b5f159794122dee5c73418d76d90be2d22b77fa
Han Wei Chia(Jingming) <h.w.chia@student.tue.nl>
2020-09-29 21:41:08 +0200

    small fixes login tests and added sidemenu redirecting tests

--- frontend/src/test/menu.test.js
+++ frontend/src/test/menu.test.js
@@ -5,7 +5,7 @@ describe('React App', () => {
   const username = "asd";
   const password = "asd";
 
-  it('succesfully go to the view planning page with homepage after logging in', async () => {
+  it('succesfully go to the user management page with the side menu after logging in', async () => {
 
     const browser = await puppeteer.launch({ 
         headless: false,
@@ -22,142 +22,143 @@ describe('React App', () => {
 
     await page.click('.ant-btn-primary');
 
-    await page.waitForSelector('#viewButton');
-    await page.click('#viewButton');
-    const currentPage = page.url();
-    expect(currentPage).toEqual('http://localhost:3000/view');
-    await page.waitFor(3000);
-    await browser.close();
-  }, 120000);
-
-  it('succesfully go to the data visualisation page with homepage after logging in', async () => {
-
-    const browser = await puppeteer.launch({ 
-        headless: false,
-        defaultViewport: null,
-        args: [`--window-size=${1920},${1080}`]
-    });
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000');
-
-    await page.waitForSelector('#basic_username');
-
-    await page.type('#basic_username', username);
-    await page.type('#basic_password', password);
-
-    await page.click('.ant-btn-primary');
-
-    await page.waitForSelector('#dataButton');
-    await page.click('#dataButton');
-    const currentPage = page.url();
-    expect(currentPage).toEqual('http://localhost:3000/data');
-    await page.waitFor(3000);
-    await browser.close();
-  }, 120000);
-
-  it('succesfully go to the monthly data analytics page with homepage after logging in', async () => {
-
-    const browser = await puppeteer.launch({ 
-        headless: false,
-        defaultViewport: null,
-        args: [`--window-size=${1920},${1080}`]
-    });
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000');
-
-    await page.waitForSelector('#basic_username');
-
-    await page.type('#basic_username', username);
-    await page.type('#basic_password', password);
-
-    await page.click('.ant-btn-primary');
-
-    await page.waitForSelector('#monthlyButton');
-    await page.click('#monthlyButton');
-    const currentPage = page.url();
-    expect(currentPage).toEqual('http://localhost:3000/montly');
-    await page.waitFor(3000);
-    await browser.close();
-  }, 120000);
-
-  it('succesfully go to the upload page with homepage after logging in', async () => {
-
-    const browser = await puppeteer.launch({ 
-        headless: false,
-        defaultViewport: null,
-        args: [`--window-size=${1920},${1080}`]
-    });
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000');
-
-    await page.waitForSelector('#basic_username');
-
-    await page.type('#basic_username', username);
-    await page.type('#basic_password', password);
-
-    await page.click('.ant-btn-primary');
-
-    await page.waitForSelector('#uploadButton');
-    await page.click('#uploadButton');
-    const currentPage = page.url();
-    expect(currentPage).toEqual('http://localhost:3000/upload');
-    await page.waitFor(3000);
-    await browser.close();
-  }, 120000);
-
-  it('succesfully go to the manual planning page with homepage after logging in', async () => {
-
-    const browser = await puppeteer.launch({ 
-        headless: false,
-        defaultViewport: null,
-        args: [`--window-size=${1920},${1080}`]
-    });
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000');
-
-    await page.waitForSelector('#basic_username');
-
-    await page.type('#basic_username', username);
-    await page.type('#basic_password', password);
-
-    await page.click('.ant-btn-primary');
-
-    await page.waitForSelector('#planningButton');
-    await page.click('#planningButton');
-    const currentPage = page.url();
-    expect(currentPage).toEqual('http://localhost:3000/planning');
-    await page.waitFor(3000);
-    await browser.close();
-  }, 120000);
-
-  
-
-  it('succesfully go to the user account management page with homepage after logging in', async () => {
-
-    const browser = await puppeteer.launch({ 
-        headless: false,
-        defaultViewport: null,
-        args: [`--window-size=${1920},${1080}`]
-    });
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000');
-
-    await page.waitForSelector('#basic_username');
-
-    await page.type('#basic_username', username);
-    await page.type('#basic_password', password);
-
-    await page.click('.ant-btn-primary');
-
-    await page.waitForSelector('#accountButton');
-    await page.click('#accountButton');
+    await page.waitForSelector('.ant-menu-item.ant-menu-item-only-child:nth-child(2)');
+    await page.click('.ant-menu-item.ant-menu-item-only-child:nth-child(2)');
+    await page.waitFor(300);
     const currentPage = page.url();
     expect(currentPage).toEqual('http://localhost:3000/account');
     await page.waitFor(3000);
     await browser.close();
-  }, 120000);
- 
+  }, 20000);
+
+  it('succesfully go to the upload page with the side menu after logging in', async () => {
+
+    const browser = await puppeteer.launch({ 
+        headless: false,
+        defaultViewport: null,
+        args: [`--window-size=${1920},${1080}`]
+    });
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+
+    await page.waitForSelector('#basic_username');
+
+    await page.type('#basic_username', username);
+    await page.type('#basic_password', password);
+
+    await page.click('.ant-btn-primary');
+
+    await page.waitForSelector('.ant-menu-item.ant-menu-item-only-child:nth-child(3)');
+    await page.click('.ant-menu-item.ant-menu-item-only-child:nth-child(3)');
+    await page.waitFor(300);
+    const currentPage = page.url();
+    expect(currentPage).toEqual('http://localhost:3000/upload');
+    await page.waitFor(3000);
+    await browser.close();
+  }, 20000);
 
 
-  
+  it('succesfully go to the manual planning page with the side menu after logging in', async () => {
+
+    const browser = await puppeteer.launch({ 
+        headless: false,
+        defaultViewport: null,
+        args: [`--window-size=${1920},${1080}`]
+    });
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+
+    await page.waitForSelector('#basic_username');
+
+    await page.type('#basic_username', username);
+    await page.type('#basic_password', password);
+
+    await page.click('.ant-btn-primary');
+
+    await page.waitForSelector('.ant-menu-item.ant-menu-item-only-child:nth-child(4)');
+    await page.click('.ant-menu-item.ant-menu-item-only-child:nth-child(4)');
+    await page.waitFor(300);
+    const currentPage = page.url();
+    expect(currentPage).toEqual('http://localhost:3000/planning');
+    await page.waitFor(3000);
+    await browser.close();
+  }, 20000);
+
+  it('succesfully go to the view planning page with the side menu after logging in', async () => {
+
+    const browser = await puppeteer.launch({ 
+        headless: false,
+        defaultViewport: null,
+        args: [`--window-size=${1920},${1080}`]
+    });
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+
+    await page.waitForSelector('#basic_username');
+
+    await page.type('#basic_username', username);
+    await page.type('#basic_password', password);
+
+    await page.click('.ant-btn-primary');
+
+    await page.waitForSelector('.ant-menu-item.ant-menu-item-only-child:nth-child(5)');
+    await page.click('.ant-menu-item.ant-menu-item-only-child:nth-child(5)');
+    await page.waitFor(300);
+    const currentPage = page.url();
+    expect(currentPage).toEqual('http://localhost:3000/view');
+    await page.waitFor(3000);
+    await browser.close();
+  }, 20000);
+
+  it('succesfully go to the data visualisation page with the side menu after logging in', async () => {
+
+    const browser = await puppeteer.launch({ 
+        headless: false,
+        defaultViewport: null,
+        args: [`--window-size=${1920},${1080}`]
+    });
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+
+    await page.waitForSelector('#basic_username');
+
+    await page.type('#basic_username', username);
+    await page.type('#basic_password', password);
+
+    await page.click('.ant-btn-primary');
+
+    await page.waitForSelector('.ant-menu-item.ant-menu-item-only-child:nth-child(6)');
+    await page.click('.ant-menu-item.ant-menu-item-only-child:nth-child(6)');
+    await page.waitFor(300);
+    const currentPage = page.url();
+    expect(currentPage).toEqual('http://localhost:3000/data');
+    await page.waitFor(3000);
+    await browser.close();
+  }, 20000);
+
+  it('succesfully go to the monthly data analytics page with the side menu after logging in', async () => {
+
+    const browser = await puppeteer.launch({ 
+        headless: false,
+        defaultViewport: null,
+        args: [`--window-size=${1920},${1080}`]
+    });
+    const page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+
+    await page.waitForSelector('#basic_username');
+
+    await page.type('#basic_username', username);
+    await page.type('#basic_password', password);
+
+    await page.click('.ant-btn-primary');
+
+    await page.waitForSelector('.ant-menu-item.ant-menu-item-only-child:nth-child(7)');
+    await page.click('.ant-menu-item.ant-menu-item-only-child:nth-child(7)');
+    await page.waitFor(300);
+    const currentPage = page.url();
+    expect(currentPage).toEqual('http://localhost:3000/montly');
+    await page.waitFor(3000);
+    await browser.close();
+  }, 20000);
 });
