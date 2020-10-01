b0c4ac52acaf892b72ef98365ee3a82de7abccb1
Rick Luiken <rick-luiken@live.nl>
2020-10-01 19:21:23 +0200

    Fix: Fixed an issue which caused new orders and truck not to be added after an order or truck had been deleted

--- backend/api/orders/resources.py
+++ backend/api/orders/resources.py
@@ -19,17 +19,29 @@ class Orders(MethodView):
     @bp.alt_response('UNAUTHORIZED', code=401)
     @bp.alt_response('NOT_FOUND', code=404)
     def get(self, sheet_id_or_latest):
+        """
+        Get a list of orders from an order sheet.
+
+        In case `sheet_id_or_latest` is `latest`, the most recently uploaded
+        order sheet will be used.
+
+        Required roles: planner, administrator
+        """
         try:
+            # Try to parse the sheet_id to an int and get the order sheet
             order_sheet = OrderSheet.query\
                 .get_or_404(int(sheet_id_or_latest),
                             description="Order sheet not found")
         except ValueError:
+            # The sheet_id is not an integer, so check if it is `latest`
             if sheet_id_or_latest == 'latest':
+                # Get the most recently uploaded sheet
                 order_sheet = OrderSheet.query\
                     .order_by(OrderSheet.upload_date.desc())\
                     .first_or_404()
             else:
-                abort(404)
+                # Can't understand which sheet is requested
+                abort(404, message='Order sheet not found')
                 return
         return order_sheet.orders
 
@@ -39,19 +51,42 @@ class Orders(MethodView):
     @bp.alt_response('UNAUTHORIZED', code=401)
     @bp.alt_response('NOT_FOUND', code=404)
     def post(self, order, sheet_id_or_latest):
+        """
+        Create a new order in an order list.
+
+        In case `sheet_id_or_latest` is `latest`, the most recently uploaded
+        order sheet will be used.
+
+        The request can contain any key value pair.
+        If the key is not known, a new field will be created for it.
+
+        Required roles: planner, administrator
+        """
         try:
+            # Try to parse the sheet_id to an int and get the order sheet
             order_sheet = OrderSheet.query \
                 .get_or_404(int(sheet_id_or_latest),
                             description="Order sheet not found")
         except ValueError:
+            # The sheet_id is not an integer, so check if it is `latest`
             if sheet_id_or_latest == 'latest':
+                # Get the most recently uploaded sheet
                 order_sheet = OrderSheet.query\
                     .order_by(OrderSheet.upload_date.desc())\
                     .first_or_404()
             else:
-                abort(404)
+                # Can't understand which sheet is requested
+                abort(404, 'Order sheet not found')
                 return
+        # Create a new order with all parameters
         new_order = Order(**order)
+
+        # Reorder the order ids, in case an order was deleted
+        # outside of the application
+        order_sheet.orders.reorder()
+        db.session.flush()
+
+        # Add the order to the order sheet
         order_sheet.add_row(new_order)
         db.session.commit()
         return new_order
@@ -66,10 +101,23 @@ class OrderByID(MethodView):
     @bp.alt_response('NOT_FOUND', code=404)
     @bp.alt_response('SERVICE_UNAVAILABLE', code=503)
     def get(self, sheet_id_or_latest, order_id):
+        """
+        Get a specific order on an order sheet.
+
+        In case `sheet_id_or_latest` is `latest`, the most recently uploaded
+        order sheet will be used.
+
+        Required roles: planner, administrator
+        """
         try:
-            order = Order.query.get_or_404((int(sheet_id_or_latest), order_id))
+            # Try to parse the sheet_id to an int and get the order
+            order = Order.query.get_or_404(
+                (int(sheet_id_or_latest), order_id),
+                description="Order not found")
         except ValueError:
+            # The sheet_id is not an integer, so check if it is `latest`
             if sheet_id_or_latest == 'latest':
+                # Check the order id on the most recently uploaded sheet
                 latest_upload = db.session.query(
                     func.max(OrderSheet.upload_date))
                 order = Order.query \
@@ -78,7 +126,8 @@ class OrderByID(MethodView):
                                  Order.order_number == order_id)) \
                     .first_or_404()
             else:
-                abort(404)
+                # Can't understand which sheet is requested
+                abort(404, message='Order not found')
                 return
         except SQLAlchemyError:
             abort(503,
@@ -87,51 +136,87 @@ class OrderByID(MethodView):
             return
         return order
 
+    # TODO: make delete idempotent
     @roles_required('planner', 'administrator')
     @bp.response(code=204)
     @bp.alt_response('UNAUTHORIZED', code=401)
     @bp.alt_response('NOT_FOUND', code=404)
     @bp.alt_response('SERVICE_UNAVAILABLE', code=503)
     def delete(self, sheet_id_or_latest, order_id):
+        """
+        Delete a specific order from an order sheet.
 
+        In case `sheet_id_or_latest` is `latest`, the most recently uploaded
+        order sheet will be used.
+
+        When an order is deleted, the other orders will get a new id assigned.
+
+        WARNING: currently this function is not idempotent.
+
+        Required roles: planner, administrator
+        """
         try:
-            order = Order.query.get_or_404(
-                (int(sheet_id_or_latest), order_id),
-                description='Order not found.')
+            # Try to parse the sheet_id to an int and get the order sheet
+            order_sheet = OrderSheet.query.get_or_404(
+                int(sheet_id_or_latest),
+                description='Order not found')
         except ValueError:
+            # The sheet_id is not an integer, so check if it is `latest`
             if sheet_id_or_latest == 'latest':
-                latest_upload = db.session.query(
-                    func.max(OrderSheet.upload_date))
-                order = Order.query \
-                    .join(OrderSheet, Order.ordersheet) \
-                    .filter(and_(OrderSheet.upload_date == latest_upload,
-                                 Order.order_number == order_id)) \
+                # Check the order id on the most recently uploaded sheet
+                order_sheet = OrderSheet.query \
+                    .order_by(OrderSheet.upload_date.desc()) \
                     .first_or_404()
             else:
-                abort(404)
+                # Can't understand which sheet is requested
+                abort(404, message='Order not found')
                 return
         except SQLAlchemyError:
             abort(503,
                   message='Something went wrong on the server.',
                   status='Service Unavailable')
             return
-        db.session.delete(order)
-        db.session.commit()
-        return "", 204
+
+        try:
+            # Delete through the order sheet, so it can rearrange the order ids
+            # of the orders
+            db.session.delete(order_sheet.orders[order_id-1])
+            db.session.flush()  # make changes to the database
+            db.session.refresh(order_sheet)  # refresh the sheet in memory
+            order_sheet.orders.reorder()  # reorder the order ids of the sheet
+            db.session.commit()
+            return "", 204
+        except IndexError:
+            abort(404, message='Order not found')
 
     @roles_required('planner', 'administrator')
     @bp.arguments(OrderSchema(partial=True))
     @bp.response(OrderSchema)
+    @bp.alt_response('BAD_REQUEST', code=400)
     @bp.alt_response('UNAUTHORIZED', code=401)
     @bp.alt_response('NOT_FOUND', code=404)
     @bp.alt_response('SERVICE_UNAVAILABLE', code=503)
     def patch(self, req, sheet_id_or_latest, order_id):
+        """
+        Change a specific order from an order sheet.
+
+        In case `sheet_id_or_latest` is `latest`, the most recently uploaded
+        order sheet will be used.
+
+        The request can contain any key value pair except the key 'others'.
+        If the key is not known, a new field will be created for it.
+
+        Required roles: planner, administrator
+        """
         try:
+            # Try to parse the sheet_id to an int and get the order
             order = Order.query.get_or_404(
                 (int(sheet_id_or_latest), order_id),
-                description='Order not found.')
+                description='Order not found')
         except ValueError:
+            # The sheet_id is not an integer, so check if it is `latest`
             if sheet_id_or_latest == 'latest':
+                # Check the order id on the most recently uploaded sheet
                 latest_upload = db.session.query(
                     func.max(OrderSheet.upload_date))
                 order = Order.query \
@@ -140,14 +225,20 @@ class OrderByID(MethodView):
                                  Order.order_number == order_id)) \
                     .first_or_404()
             else:
+                # Can't understand which sheet is requested
                 abort(404)
                 return
 
         try:
+            # Iterate over the keys and values in the request
             for k, v in req.items():
                 if k != "others" and hasattr(order, k):
+                    # If the key has a column (except 'others')
+                    # in the database, change it
                     setattr(order, k, v)
                 else:
+                    # If the key doesn't have column,
+                    # place it in the other columns
                     order.others[k] = v
             db.session.commit()
             return order, 200
--- backend/api/trucks/resources.py
+++ backend/api/trucks/resources.py
@@ -15,16 +15,29 @@ class Trucks(MethodView):
     @bp.response(TruckSchema(many=True))
     @bp.alt_response('NOT_FOUND', code=404)
     def get(self, sheet_id_or_latest):
+        """
+        Get a list of truck from a truck sheet.
+
+        In case `sheet_id_or_latest` is `latest`, the most recently uploaded
+        truck sheet will be used.
+
+        Required roles: planner, administrator
+        """
         try:
+            # Try to parse the sheet_id to an int and get the truck sheet
             truck_sheet = TruckSheet.query\
-                .get_or_404(int(sheet_id_or_latest))
+                .get_or_404(int(sheet_id_or_latest),
+                            description='Truck sheet not found')
         except ValueError:
+            # The sheet_id is not an integer, so check if it is `latest`
             if sheet_id_or_latest == 'latest':
+                # Get the most recently uploaded sheet
                 truck_sheet = TruckSheet.query\
                     .order_by(TruckSheet.upload_date.desc())\
                     .first_or_404()
             else:
-                abort(404)
+                # Can't understand which sheet is requested
+                abort(404, message='Truck sheet not found')
                 return
         return truck_sheet.trucks
 
@@ -32,19 +45,42 @@ class Trucks(MethodView):
     @bp.arguments(TruckSchema)
     @bp.response(TruckSchema)
     def post(self, truck, sheet_id_or_latest):
+        """
+        Create a new truck in a truck list.
+
+        In case `sheet_id_or_latest` is `latest`, the most recently uploaded
+        truck sheet will be used.
+
+        The request can contain any key value pair.
+        If the key is not known, a new field will be created for it.
+
+        Required roles: planner, administrator
+        """
         try:
-            truck_sheet = TruckSheet.query\
+            # Try to parse the sheet_id to an int and get the truck sheet
+            truck_sheet = TruckSheet.query \
                 .get_or_404(int(sheet_id_or_latest),
-                            description="Order sheet not found")
+                            description='Truck sheet not found')
         except ValueError:
+            # The sheet_id is not an integer, so check if it is `latest`
             if sheet_id_or_latest == 'latest':
-                truck_sheet = TruckSheet.query\
-                    .order_by(TruckSheet.upload_date.desc())\
+                # Get the most recently uploaded sheet
+                truck_sheet = TruckSheet.query \
+                    .order_by(TruckSheet.upload_date.desc()) \
                     .first_or_404()
             else:
-                abort(404)
+                # Can't understand which sheet is requested
+                abort(404, message='Truck sheet not found')
                 return
+        # Create a new truck with all parameters
         new_truck = Truck(**truck)
+
+        # Reorder the truck ids, in case an order was deleted
+        # outside of the application
+        truck_sheet.trucks.reorder()
+        db.session.flush()
+
+        # Add the truck to the truck sheet
         truck_sheet.add_row(new_truck)
         db.session.commit()
         return new_truck
@@ -59,10 +95,23 @@ class TruckByID(MethodView):
     @bp.alt_response('NOT_FOUND', code=404)
     @bp.alt_response('SERVICE_UNAVAILABLE', code=503)
     def get(self, sheet_id_or_latest, truck_id):
+        """
+        Get a specific truck on a truck sheet.
+
+        In case `sheet_id_or_latest` is `latest`, the most recently uploaded
+        truck sheet will be used.
+
+        Required roles: planner, administrator
+        """
         try:
-            truck = Truck.query.get_or_404((int(sheet_id_or_latest), truck_id))
+            # Try to parse the sheet_id to an int and get the truck
+            truck = Truck.query.get_or_404(
+                (int(sheet_id_or_latest), truck_id),
+                description='Truck not found')
         except ValueError:
+            # The sheet_id is not an integer, so check if it is `latest`
             if sheet_id_or_latest == 'latest':
+                # Check the truck id on the most recently uploaded sheet
                 latest_upload = db.session.query(
                     func.max(TruckSheet.upload_date))
                 truck = Truck.query \
@@ -71,7 +120,8 @@ class TruckByID(MethodView):
                                  Truck.s_number == truck_id)) \
                     .first_or_404()
             else:
-                abort(404)
+                # Can't understand which sheet is requested
+                abort(404, message='Truck not found')
                 return
         except SQLAlchemyError:
             abort(503,
@@ -80,34 +130,58 @@ class TruckByID(MethodView):
             return
         return truck
 
+    # TODO: make delete idempotent
     @roles_required('planner', 'administrator')
     @bp.response(code=204)
     @bp.alt_response('UNAUTHORIZED', code=401)
     @bp.alt_response('NOT_FOUND', code=404)
     @bp.alt_response('SERVICE_UNAVAILABLE', code=503)
     def delete(self, sheet_id_or_latest, truck_id):
+        """
+        Delete a specific truck from a truck sheet.
+
+        In case `sheet_id_or_latest` is `latest`, the most recently uploaded
+        truck sheet will be used.
+
+        When a truck is deleted, the other trucks will get a new id assigned.
+
+        WARNING: currently this function is not idempotent.
+
+        Required roles: planner, administrator
+        """
         try:
-            truck = Truck.query.get_or_404((int(sheet_id_or_latest), truck_id))
+            # Try to parse the sheet_id to an int and get the truck sheet
+            truck_sheet = TruckSheet.query.get_or_404(
+                int(sheet_id_or_latest),
+                description='Truck not found')
         except ValueError:
+            # The sheet_id is not an integer, so check if it is `latest`
             if sheet_id_or_latest == 'latest':
-                latest_upload = db.session.query(
-                    func.max(TruckSheet.upload_date))
-                truck = Truck.query \
-                    .join(TruckSheet, Truck.trucksheet) \
-                    .filter(and_(TruckSheet.upload_date == latest_upload,
-                                 Truck.s_number == truck_id)) \
+                # Check the truck id on the most recently uploaded sheet
+                truck_sheet = TruckSheet.query \
+                    .order_by(TruckSheet.upload_date.desc()) \
                     .first_or_404()
             else:
-                abort(404)
+                # Can't understand which sheet is requested
+                abort(404, message='Truck not found')
                 return
         except SQLAlchemyError:
             abort(503,
                   message='Something went wrong on the server.',
                   status='Service Unavailable')
             return
-        db.session.delete(truck)
-        db.session.commit()
-        return "", 204
+
+        try:
+            # Delete through the truck sheet, so it can rearrange the truck
+            # no's of the trucks
+            db.session.delete(truck_sheet.trucks[truck_id - 1])
+            db.session.flush()  # make changes to the database
+            db.session.refresh(truck_sheet)  # refresh the sheet in memory
+            truck_sheet.trucks.reorder()  # reorder the truck no's of the sheet
+            db.session.commit()
+            return "", 204
+        except IndexError:
+            abort(404, message='Truck not found')
 
     @roles_required('planner', 'administrator')
     @bp.arguments(TruckSchema(partial=True))
@@ -116,10 +190,24 @@ class TruckByID(MethodView):
     @bp.alt_response('NOT_FOUND', code=404)
     @bp.alt_response('SERVICE_UNAVAILABLE', code=503)
     def patch(self, req, sheet_id_or_latest, truck_id):
+        """
+        Change a specific truck from a truck sheet.
+
+        In case `sheet_id_or_latest` is `latest`, the most recently uploaded
+        truck sheet will be used.
+
+        The request can contain any key value pair except the key 'others'.
+        If the key is not known, a new field will be created for it.
+
+        Required roles: planner, administrator
+        """
         try:
+            # Try to parse the sheet_id to an int and get the truck
             truck = Truck.query.get_or_404((int(sheet_id_or_latest), truck_id))
         except ValueError:
+            # The sheet_id is not an integer, so check if it is `latest`
             if sheet_id_or_latest == 'latest':
+                # Check the truck id on the most recently uploaded sheet
                 latest_upload = db.session.query(
                     func.max(TruckSheet.upload_date))
                 truck = Truck.query \
@@ -128,14 +216,20 @@ class TruckByID(MethodView):
                                  Truck.s_number == truck_id)) \
                     .first_or_404()
             else:
+                # Can't understand which sheet is requested
                 abort(404)
                 return
 
         try:
+            # Iterate over the keys and values in the request
             for k, v in req.items():
                 if k != "others" and hasattr(truck, k):
+                    # If the key has a column (except 'others')
+                    # in the database, change it
                     setattr(truck, k, v)
                 else:
+                    # If the key doesn't have column,
+                    # place it in the other columns
                     truck.others[k] = v
             db.session.commit()
             return truck, 200
