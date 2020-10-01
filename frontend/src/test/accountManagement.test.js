b74f23ff20a7cbe37af5205314c20c44ee7d0447
Saltimbancul <32740860+Saltimbancul@users.noreply.github.com>
2020-10-01 16:04:18 +0200

    error message added

--- frontend/src/test/accountManagement.test.js
+++ frontend/src/test/accountManagement.test.js
@@ -54,6 +54,43 @@ describe('React App', () => {
       }, 120000);
 
 
+      it('shall succesfully display error message on empty fields an account creation', async () => {
+
+        const browser = await puppeteer.launch({ 
+            headless: false,
+            defaultViewport: null,
+            args: [`--window-size=${1920},${1080}`]
+        });
+        const page = await browser.newPage();
+        await page.goto('http://localhost:3000');
+    
+        await page.waitForSelector('#basic_username');
+    
+        await page.type('#basic_username', username);
+        await page.type('#basic_password', password);
+    
+        await page.click('.ant-btn-primary');
+    
+        await page.waitForSelector('.ant-menu-item.ant-menu-item-only-child:nth-child(2)');
+        await page.click('.ant-menu-item.ant-menu-item-only-child:nth-child(2)');
+        await page.waitFor(300);
+        const currentPage = page.url();
+        expect(currentPage).toEqual('http://localhost:3000/account');
+
+        
+        await page.click('.ant-btn.ant-btn-primary');
+
+
+        await page.click('#nest-messages > div:nth-child(5) > div > div > div > button');
+
+        await page.waitForSelector('.ant-message-custom-content.ant-message-error');
+        const ErrorMessage = await page.$eval('.ant-message-custom-content.ant-message-error', element => element.textContent);
+
+        expect(ErrorMessage).toEqual('Failed: Please complete all the required fields');
+
+      }, 120000);
+
+
       it('shall succesfully edit an account', async () => {
 
         const browser = await puppeteer.launch({ 
@@ -97,6 +134,45 @@ describe('React App', () => {
         const ErrorMessage = await page.$eval('.ant-message-custom-content.ant-message-success', element => element.textContent);
 
         expect(ErrorMessage).toEqual(`Account updated succesfully`);
+
+      }, 120000);
+
+
+
+      it('shall succesfully display error message on empty fields an account editiong', async () => {
+
+        const browser = await puppeteer.launch({ 
+            headless: false,
+            defaultViewport: null,
+            args: [`--window-size=${1920},${1080}`]
+        });
+        const page = await browser.newPage();
+        await page.goto('http://localhost:3000');
+    
+        await page.waitForSelector('#basic_username');
+    
+        await page.type('#basic_username', username);
+        await page.type('#basic_password', password);
+    
+        await page.click('.ant-btn-primary');
+    
+        await page.waitForSelector('.ant-menu-item.ant-menu-item-only-child:nth-child(2)');
+        await page.click('.ant-menu-item.ant-menu-item-only-child:nth-child(2)');
+        await page.waitFor(300);
+        const currentPage = page.url();
+        expect(currentPage).toEqual('http://localhost:3000/account');
+
+        
+        await page.click('#root > section > section > section > section > main > section > section > section:nth-child(3) > section > div > div > div > div > div > ul > li:nth-child(4) > div:nth-child(2) > button:nth-child(1)');
+
+
+
+        await page.click('#nest-messages > div:nth-child(5) > div > div > div > button');
+
+        await page.waitForSelector('.ant-message-custom-content.ant-message-error');
+        const ErrorMessage = await page.$eval('.ant-message-custom-content.ant-message-error', element => element.textContent);
+
+        expect(ErrorMessage).toEqual('Failed: Please complete all the required fields');
 
       }, 120000);
 
