49226f12819eea2bcfcd4ac41c67c2c82a91c1d9
Han Wei Chia(Jingming) <h.w.chia@student.tue.nl>
2020-10-05 15:29:41 +0200

    Feature: Tooltip html working. To Do: Tooltip Html styling

--- frontend/src/components/DataVisualization/DataVisualization.js
+++ frontend/src/components/DataVisualization/DataVisualization.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 import Chart from "react-google-charts";
 
+function createCustomHTMLTooltip(orderID,duration,startTime,endTime){
+  return('<div>'+
+  '<b>'+ orderID +'</b>'+
+  '<b>'+ duration +'</b>'+
+  '<b>'+ startTime+ '</b>'+
+  '<b>'+ endTime +'</b>'+
+  '</div>'
+  )
+}
+
+
+
 export default class DataVisualization extends Component {
     render(){
       return(
@@ -13,132 +25,78 @@ export default class DataVisualization extends Component {
     [
       { type: 'string', id: 'Room' },
       { type: 'string', id: 'Name' },
+      { type: 'string', role: 'tooltip', 'p': {'html': true}},
       { type: 'date', id: 'Start' },
       { type: 'date', id: 'End' },
     ],
       [
       'Truck 1',
       'order id',
+      '<b>hey i like to code</b>',
       new Date(0, 0, 0, 8, 0, 0),
       new Date(0, 0, 0, 10, 30, 0),
     ],
     [
       'Truck 1',
       'order id',
+      createCustomHTMLTooltip('Order 123','2 Hours','8:00','10:30'),
       new Date(0, 0, 0, 12, 0, 0),
       new Date(0, 0, 0, 13, 30, 0),
     ],
     [
       'Truck 1',
       'order id',
+      createCustomHTMLTooltip('Order 123','2 Hours','8:00','10:30'),
       new Date(0, 0, 0, 14, 0, 0),
       new Date(0, 0, 0, 15, 30, 0),
     ],
     [
       'Truck 1',
       'order id',
+      createCustomHTMLTooltip('Order 123','2 Hours','8:00','10:30'),
       new Date(0, 0, 0, 16, 0, 0),
       new Date(0, 0, 0, 17, 30, 0),
     ],
     [
       'Truck 2',
       'order id',
+      createCustomHTMLTooltip('Order 123','2 Hours','8:00','10:30'),
       new Date(0, 0, 0, 12, 30, 0),
       new Date(0, 0, 0, 14, 0, 0),
     ],
     [
       'Truck 2',
       'order id',
+      createCustomHTMLTooltip('Order123',new Date(0, 0, 0, 14, 30, 0),'8:00','10:30'),
       new Date(0, 0, 0, 14, 30, 0),
       new Date(0, 0, 0, 16, 0, 0),
     ],
     [
       'Truck 2',
       'order id',
+      createCustomHTMLTooltip('Order 123','2 Hours','8:00','10:30'),
       new Date(0, 0, 0, 16, 30, 0),
       new Date(0, 0, 0, 18, 0, 0),
     ],
       [
       'Truck 3',
       'order id',
+      createCustomHTMLTooltip('Order 123','2 Hours','8:00','10:30'),
       new Date(0, 0, 0, 9, 30, 0),
       new Date(0, 0, 0, 11, 0, 0),
     ],
       [
       'Truck 3',
       'order id',
+      createCustomHTMLTooltip('Order 123','2 Hours','8:00','10:30'),
       new Date(0, 0, 0, 15, 30, 0),
       new Date(0, 0, 0, 18, 0, 0),
-    ],
-
-      [
-      'Truck 4',
-      'order id',
-      new Date(0, 0, 0, 8, 30, 0),
-      new Date(0, 0, 0, 11, 0, 0),
-    ],
-      [
-      'Truck 4',
-      'order id',
-      new Date(0, 0, 0, 12, 30, 0),
-      new Date(0, 0, 0, 15, 0, 0),
-    ],
-
-      [
-      'Truck 5',
-      'order id',
-      new Date(0, 0, 0, 9, 30, 0),
-      new Date(0, 0, 0, 11, 0, 0),
-    ],
-      [
-      'Truck 5',
-      'order id',
-      new Date(0, 0, 0, 12, 30, 0),
-      new Date(0, 0, 0, 14, 0, 0),
-    ],
-
-      [
-      'Truck 6',
-      'order id',
-      new Date(0, 0, 0, 10, 30, 0),
-      new Date(0, 0, 0, 12, 0, 0),
-    ],
-      [
-      'Truck 6',
-      'order id',
-      new Date(0, 0, 0, 15, 30, 0),
-      new Date(0, 0, 0, 16, 0, 0),
-    ],
-
-      [
-      'Truck 7',
-      'order id',
-      new Date(0, 0, 0, 8, 30, 0),
-      new Date(0, 0, 0, 10, 0, 0),
-    ],
-      [
-      'Truck 7',
-      'order id',
-      new Date(0, 0, 0, 14, 30, 0),
-      new Date(0, 0, 0, 17, 0, 0),
-    ],
-
-      [
-      'Truck 8',
-      'order id',
-      new Date(0, 0, 0, 8, 30, 0),
-      new Date(0, 0, 0, 11, 0, 0),
-    ],
-      [
-      'Truck 9',
-      'order id',
-      new Date(0, 0, 0, 12, 30, 0),
-      new Date(0, 0, 0, 15, 0, 0),
     ],
   ]}
   options={{
     timeline: {
       colorByRowLabel: true,
+      allowHtml: true,
     },
   }}
   rootProps={{ 'data-testid': '5' }}
