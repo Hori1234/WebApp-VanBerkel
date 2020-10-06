fd5a91c7970eab34a2e15cb619dd7dc9c4842243
Han Wei Chia(Jingming) <h.w.chia@student.tue.nl>
2020-10-06 14:36:01 +0200

    Feutures: Tooltip Styling, Create Tooltip, Create time dates, Create single data input for 1 order.

--- frontend/src/components/DataVisualization/DataVisualization.js
+++ frontend/src/components/DataVisualization/DataVisualization.js
@@ -1,104 +1,124 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Chart from "react-google-charts";
+import "../Css/DataVisualization.css";
 
-function createCustomHTMLTooltip(orderID, startTime, endTime, destination) {
-    return ('<div>' +
-        '<b>' + orderID + '</b><br>' +
-        '<b>' + startTime + '</b><br>' +
-        '<b>' + endTime + '<b/><br>' +
-        '<b>' + destination + '<b/><br>' +
-        '</div>'
-    )
+// creates the tooltip of an order
+function createCustomHTMLTooltip(orderID, startTime, endTime,duration, destination) {
+  return (
+            '<ul class="flex-container">' +
+                '<li class="flex-item" style="text-align: center" ><h4><b>' + orderID + '</b></h4></li>' +
+                '<li class="flex-item"><b>Start: </b>' + startTime + '</li>' +
+                '<li class="flex-item"><b>End: </b>' + endTime + '</li>' +
+                '<li class="flex-item"><b>Duration: </b>' + duration + '</li>' +
+                '<li class="flex-item"><b>Destination: </b>' + destination + '</li>' +
+            '</ul>'
+  )
+}
+
+//create javascript data object
+function createDataTime(time){
+  let index = time.indexOf(":");
+  let hours = time.substr(0, index)
+  let minutes = time.substr(index+1)
+  return(
+    new Date(0, 0, 0, hours, minutes, 0)
+  )
+}
+
+// create single data input for timeline
+function createSingleDataInput(truckID,orderID, startTime, endTime,duration, destination) {
+  return([truckID,
+    orderID,
+  createCustomHTMLTooltip(orderID, startTime, endTime,duration, destination),
+  createDataTime(startTime),
+  createDataTime(endTime),
+  ])
+}
+
+// create all the data input for timeline
+function createAllDataInput(truckID,orderID, startTime, endTime,duration, destination){
+
 }
 
 export default class DataVisualization extends Component {
-    render() {
-        return (
-            <Chart
-                width={'100%'}
-                height={'100%'}
-                chartType="Timeline"
-                loader={<div>Loading Chart</div>}
-                data={[
-                    [
-                        {type: 'string', id: 'Room'},
-                        {type: 'string', id: 'Name'},
-                        {type: 'string', role: 'tooltip', 'p': {'html': true}},
-                        {type: 'date', id: 'Start'},
-                        {type: 'date', id: 'End'},
-                    ],
-                    [
-                        'Truck 1',
-                        'order id',
-                        createCustomHTMLTooltip('Order #:123', 'Start :8:00', 'End:10:30', 'Destination:X'),
-                        new Date(0, 0, 0, 8, 0, 0),
-                        new Date(0, 0, 0, 10, 30, 0),
-                    ],
-                    [
-                        'Truck 1',
-                        'order id',
-                        createCustomHTMLTooltip('Order #:123', 'Start :8:00', 'End:10:30', 'Destination:X'),
-                        new Date(0, 0, 0, 12, 0, 0),
-                        new Date(0, 0, 0, 13, 30, 0),
-                    ],
-                    [
-                        'Truck 1',
-                        'order id',
-                        createCustomHTMLTooltip('Order #:123', 'Start :8:00', 'End:10:30', 'Destination:X'),
-                        new Date(0, 0, 0, 14, 0, 0),
-                        new Date(0, 0, 0, 15, 30, 0),
-                    ],
-                    [
-                        'Truck 1',
-                        'order id',
-                        createCustomHTMLTooltip('Order #:123', 'Start :8:00', 'End:10:30', 'Destination:X'),
-                        new Date(0, 0, 0, 16, 0, 0),
-                        new Date(0, 0, 0, 17, 30, 0),
-                    ],
-                    [
-                        'Truck 2',
-                        'order id',
-                        createCustomHTMLTooltip('Order #:123', 'Start :8:00', 'End:10:30', 'Destination:X'),
-                        new Date(0, 0, 0, 12, 30, 0),
-                        new Date(0, 0, 0, 14, 0, 0),
-                    ],
-                    [
-                        'Truck 2',
-                        'order id',
-                        createCustomHTMLTooltip('Order #:123', 'Start :8:00', 'End:10:30', 'Destination:X'),
-                        new Date(0, 0, 0, 14, 30, 0),
-                        new Date(0, 0, 0, 16, 0, 0),
-                    ],
-                    [
-                        'Truck 2',
-                        'order id',
-                        createCustomHTMLTooltip('Order #:123', 'Start :8:00', 'End:10:30', 'Destination:X'),
-                        new Date(0, 0, 0, 16, 30, 0),
-                        new Date(0, 0, 0, 18, 0, 0),
-                    ],
-                    [
-                        'Truck 3',
-                        'order id',
-                        createCustomHTMLTooltip('Order #:123', 'Start :8:00', 'End:10:30', 'Destination:X'),
-                        new Date(0, 0, 0, 9, 30, 0),
-                        new Date(0, 0, 0, 11, 0, 0),
-                    ],
-                    [
-                        'Truck 3',
-                        'order id',
-                        createCustomHTMLTooltip('Order #:123', 'Start :8:00', 'End:10:30', 'Destination:X'),
-                        new Date(0, 0, 0, 15, 30, 0),
-                        new Date(0, 0, 0, 18, 0, 0),
-                    ],
-                ]}
-                options={{
-                    timeline: {
-                        colorByRowLabel: true,
-                        allowHtml: true,
-                    },
-                }}
-                rootProps={{'data-testid': '5'}}
-            />
-        )
-    }
+  render() {
+    return (
+      <Chart
+        width={'100%'}
+        height={'100%'}
+        chartType="Timeline"
+        loader={<div>Loading Chart</div>}
+        data={[
+          [
+            { type: 'string', id: 'Room' },
+            { type: 'string', id: 'Name' },
+            { type: 'string', role: 'tooltip', 'p': { 'html': true } },
+            { type: 'date', id: 'Start' },
+            { type: 'date', id: 'End' },
+          ],
+            createSingleDataInput('Truck 1','167639B','8:00', '10:30', '2:30', 'Amsterdam')
+          ,
+            createSingleDataInput('Truck 1','167639B','10:30', '12:30', '2:30', 'Eindhoven')
+          ,
+          [
+            'Truck 1',
+            'order id',
+            createCustomHTMLTooltip('167639B','8:00', '10:30', '2:30', 'Eindhoven'),
+            new Date(0, 0, 0, 14, 0, 0),
+            new Date(0, 0, 0, 15, 30, 0),
+          ],
+          [
+            'Truck 1',
+            'order id',
+            createCustomHTMLTooltip('167639B','8:00', '10:30', '2:30', 'Eindhoven'),
+            new Date(0, 0, 0, 16, 0, 0),
+            new Date(0, 0, 0, 17, 30, 0),
+          ],
+          [
+            'Truck 2',
+            'order id',
+            createCustomHTMLTooltip('167639B','8:00', '10:30', '2:30', 'Eindhoven'),
+            new Date(0, 0, 0, 12, 30, 0),
+            new Date(0, 0, 0, 14, 0, 0),
+          ],
+          [
+            'Truck 2',
+            'order id',
+            createCustomHTMLTooltip('167639B','8:00', '10:30', '2:30', 'Eindhoven'),
+            new Date(0, 0, 0, 14, 30, 0),
+            new Date(0, 0, 0, 16, 0, 0),
+          ],
+          [
+            'Truck 2',
+            'order id',
+            createCustomHTMLTooltip('167639B','8:00', '10:30', '2:30', 'Eindhoven'),
+            new Date(0, 0, 0, 16, 30, 0),
+            new Date(0, 0, 0, 18, 0, 0),
+          ],
+          [
+            'Truck 3',
+            'order id',
+            createCustomHTMLTooltip('167639B','8:00', '10:30', '2:30', 'Eindhoven'),
+            new Date(0, 0, 0, 9, 30, 0),
+            new Date(0, 0, 0, 11, 0, 0),
+          ],
+          [
+            'Truck 3',
+            'order id',
+            createCustomHTMLTooltip('167639B','8:00', '10:30', '2:30', 'Eindhoven'),
+            new Date(0, 0, 0, 15, 30, 0),
+            new Date(0, 0, 0, 18, 0, 0),
+          ],
+        ]}
+        options={{
+          timeline: {
+            colorByRowLabel: true,
+            allowHtml: true,
+          },
+          avoidOverlappingGridLines: true,
+        }}
+        rootProps={{ 'data-testid': '5' }}
+      />
+    )
+  }
 }
