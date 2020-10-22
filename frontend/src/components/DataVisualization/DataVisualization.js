6dd9717283fdcca4026fc9e01b39b518195f491d
zakariaameziane <zakaria.ameziane01@gmail.com>
2020-10-22 09:19:37 +0200

    pop up confirmation for publishing

--- frontend/src/components/DataVisualization/DataVisualization.js
+++ frontend/src/components/DataVisualization/DataVisualization.js
@@ -1,55 +1,62 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Timeline from "./Timeline"
 import FirstRideButton from "./FirstRideButton"
 import "../Css/DataVisualization.css";
-import { Row, Col, Layout, Button, message} from "antd";
+import {Row, Col, Layout, Button, message, Popconfirm} from "antd";
 import "antd/dist/antd.css";
 import axios from "axios";
 
 export default class DataVisualization extends Component {
-  
-  publishLatest = (value,value1) => {
-    return axios
-      .post(`/api/plannings/${value}/${value1}`)
-      .then((res) => {
-        if (res.status === 200) {
-          message.success("Planning: Published successfully");
-        }
-        return true;
-      })
-      .catch((error) => {
-        this.setState((state) => ({
-          ...state,
-          status: "error",
-          error: error,
-        }));
-          message.error("Unauthorized: " + error.response.data.message);
-          return false;
-      });
-  };
 
-  render() {
-    return (
-      <Layout
-        style={{
-          display: "flex",
-          height: "100%",
-          width: "100%",
-          background: "white"
-        }}
-      >
-        <Timeline timeline="latest" />
-        <Row>
-          <Col span={6}>
-            <FirstRideButton orderNumber="latest" />
-          </Col>
-          <Col span={4} offset={14}>
-            <Button type="primary" size={"large"} style={{ width: "100%" }} onClick={() => this.publishLatest('latest', 'latest')}>
-              Publish
-            </Button>
-          </Col>
-        </Row>
-      </Layout>
-    );
-  }
+    publishLatest = (value, value1) => {
+        return axios
+            .post(`/api/plannings/${value}/${value1}`)
+            .then((res) => {
+                if (res.status === 200) {
+                    message.success("Planning: Published successfully");
+                }
+                return true;
+            })
+            .catch((error) => {
+                this.setState((state) => ({
+                    ...state,
+                    status: "error",
+                    error: error,
+                }));
+                message.error("Unauthorized: " + error.response.data.message);
+                return false;
+            });
+    };
+
+    render() {
+        return (
+            <Layout
+                style={{
+                    display: "flex",
+                    height: "100%",
+                    width: "100%",
+                    background: "white"
+                }}
+            >
+                <Timeline timeline="latest"/>
+                <Row>
+                    <Col span={6}>
+                        <FirstRideButton orderNumber="latest"/>
+                    </Col>
+                    <Col span={4} offset={14}>
+                        <Popconfirm
+                            title="Are you sure you want to publish this planning?"
+                            onConfirm={() => this.publishLatest('latest', 'latest')}
+                            okText="Yes"
+                            cancelText="No"
+                        >
+                            <Button type="primary" size={"large"} style={{width: "100%"}}>
+                                Publish
+                            </Button>
+                        </Popconfirm>,
+                    </Col>
+                </Row>
+            </Layout>
+        );
+    }
 }
